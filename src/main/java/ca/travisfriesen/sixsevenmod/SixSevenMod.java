081733722986475cb9497efb63964e19fbc3e5d1
travisfriesen <github@travisfriesen.ca>
2025-11-08 11:12:06 -0600

    feat: initial commit

--- src/main/java/ca/travisfriesen/sixsevenmod/SixSevenMod.java
+++ src/main/java/ca/travisfriesen/sixsevenmod/SixSevenMod.java
@@ -1,12 +1,12 @@
-package com.example;
+package ca.travisfriesen.sixsevenmod;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class SixSevenMod implements ModInitializer {
+	public static final String MOD_ID = "67mod";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
